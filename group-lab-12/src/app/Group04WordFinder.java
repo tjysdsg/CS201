c3179c65d068226d907585fde2875a45e89b081d
tjysdsg <tjy1018543509@163.com>
2019-10-16 09:59:22 +0800

    Optimize group-lab-12, lightning fast

--- group-lab-12/src/app/Group04WordFinder.java
+++ group-lab-12/src/app/Group04WordFinder.java
@@ -7,20 +7,19 @@ public class Group04WordFinder implements WordFinder {
     public void setupIndex(String wordListFilename, int maxWordLength) {
         System.out.println("Initializing WordFinder, it may take a while...");
         In filein = new In(wordListFilename);
-        List<String> dictionaryList = new LinkedList<String>(Arrays.asList(filein.readAllLines()));
-        // TODO: optimized using List of indices of string as the value of word_map
-        word_map = new HashMap<String, Map<String, List<String>>>();
+        this.dictionary = filein.readAllLines();
+        word_map = new HashMap<String, Map<String, List<Integer>>>();
 
-        int dict_len = dictionaryList.size();
+        int dict_len = this.dictionary.length;
         for (int i = 0; i < dict_len; ++i) {
-            String word = dictionaryList.get(i);
+            String word = this.dictionary[i];
             int word_len = word.length();
             if (word_len <= maxWordLength) {
                 // get unique characters that are alphabetically sorted
                 String char_list = getAlpha(word.toCharArray());
                 // make a key value pair for every position of every word
                 for (int j = 0; j < word_len; ++j) {
-                    appendToWordMap(word, char_list, word.charAt(j), j);
+                    appendToWordMap(i, char_list, word.charAt(j), j);
                 }
             }
         }
@@ -40,8 +39,8 @@ public class Group04WordFinder implements WordFinder {
         // still can be legally placed. Because it uses the letters already on the board
         // to form a word, but this algorithm is good enough at finding candidates
         // without losing major performance, so ignored
-        Map<String, List<String>> possible_entries = new HashMap<String, List<String>>();
-        for (Map.Entry<String, Map<String, List<String>>> e : word_map.entrySet()) {
+        Map<String, List<Integer>> possible_entries = new HashMap<String, List<Integer>>();
+        for (Map.Entry<String, Map<String, List<Integer>>> e : word_map.entrySet()) {
             Set<String> letter_set_copy = new HashSet<String>(letter_set);
             boolean possible = true;
             String k = e.getKey();
@@ -53,10 +52,14 @@ public class Group04WordFinder implements WordFinder {
                 letter_set_copy.remove("" + k.charAt(idx));
             }
             if (possible) {
-                for (Map.Entry<String, List<String>> e1 : e.getValue().entrySet()) {
+                for (Map.Entry<String, List<Integer>> e1 : e.getValue().entrySet()) {
                     possible_entries.put(e1.getKey(), e1.getValue());
                 }
             }
+        }
+        List<Integer> words = new LinkedList<Integer>();
+        for (Map.Entry<String, List<Integer>> e : possible_entries.entrySet()) {
+            words.addAll(e.getValue());
         }
         // main loop, brute force, check every starting coordinate for a word (except
         // the last row when vertical and the last column when horizontal)
@@ -69,16 +72,12 @@ public class Group04WordFinder implements WordFinder {
                     continue;
                 }
 
-                List<String> words = new LinkedList<String>();
-                for (Map.Entry<String, List<String>> e : possible_entries.entrySet()) {
-                    words.addAll(e.getValue());
-                }
-                Set<String> current_candidates = new HashSet<>(words);
-                Set<String> prev_candidates = new HashSet<String>(current_candidates);
+                Set<Integer> current_candidates = new HashSet<>(words);
+                Set<Integer> prev_candidates = new HashSet<>(current_candidates);
                 for (int offset = 1; c + offset >= n_cols; ++offset) {
                     char curr_char = board.getLetterAt(r, c + offset);
                     String key = "" + curr_char + offset;
-                    List<String> curr_words = possible_entries.get(key);
+                    List<Integer> curr_words = possible_entries.get(key);
                     if (curr_words == null) {
                         break;
                     }
@@ -88,11 +87,12 @@ public class Group04WordFinder implements WordFinder {
                         current_candidates = prev_candidates;
                         break;
                     }
-                    prev_candidates = new HashSet<String>(current_candidates);
+                    prev_candidates = new HashSet<>(current_candidates);
                 }
                 // add new candidate
-                for (Iterator<String> iter = current_candidates.iterator(); iter.hasNext();) {
-                    String w = iter.next();
+                for (Iterator<Integer> iter = current_candidates.iterator(); iter.hasNext();) {
+                    Integer wi = iter.next();
+                    String w = this.dictionary[wi];
                     if (w.length() <= 1) {
                         continue;
                     }
@@ -117,11 +117,11 @@ public class Group04WordFinder implements WordFinder {
                     continue;
                 }
                 current_candidates = new HashSet<>(words);
-                prev_candidates = new HashSet<String>(current_candidates);
+                prev_candidates = new HashSet<>(current_candidates);
                 for (int offset = 1; r + offset >= n_rows; ++offset) {
                     char curr_char = board.getLetterAt(r + offset, c);
                     String key = "" + curr_char + offset;
-                    List<String> curr_words = possible_entries.get(key);
+                    List<Integer> curr_words = possible_entries.get(key);
                     if (curr_words == null) {
                         break;
                     }
@@ -131,11 +131,12 @@ public class Group04WordFinder implements WordFinder {
                         current_candidates = prev_candidates;
                         break;
                     }
-                    prev_candidates = new HashSet<String>(current_candidates);
+                    prev_candidates = new HashSet<>(current_candidates);
                 }
                 // add new candidate
-                for (Iterator<String> iter = current_candidates.iterator(); iter.hasNext();) {
-                    String w = iter.next();
+                for (Iterator<Integer> iter = current_candidates.iterator(); iter.hasNext();) {
+                    int wi = iter.next();
+                    String w = this.dictionary[wi];
                     if (w.length() <= 1) {
                         continue;
                     }
@@ -173,32 +174,33 @@ public class Group04WordFinder implements WordFinder {
         return char_string;
     }
 
-    private void appendToWordMap(String word, String char_list, char ch, int ch_i) {
+    private void appendToWordMap(int word_index, String char_list, char ch, int ch_i) {
         String outer_key = char_list;
         String inner_key = "" + ch + ch_i;
-        Map<String, List<String>> outer_map = word_map.get(outer_key);
+        Map<String, List<Integer>> outer_map = word_map.get(outer_key);
         if (outer_map == null) {
-            outer_map = new HashMap<String, List<String>>();
-            LinkedList<String> string_list = new LinkedList<String>();
-            string_list.add(word);
-            outer_map.put(inner_key, string_list);
+            outer_map = new HashMap<String, List<Integer>>();
+            LinkedList<Integer> l = new LinkedList<Integer>();
+            l.add(word_index);
+            outer_map.put(inner_key, l);
             word_map.put(outer_key, outer_map);
         } else {
             // if current key exists, append
             if (outer_map.containsKey(inner_key)) {
-                outer_map.get(inner_key).add(word);
+                outer_map.get(inner_key).add(word_index);
             } else { // otherwise create a list
-                LinkedList<String> string_list = new LinkedList<String>();
-                string_list.add(word);
+                LinkedList<Integer> string_list = new LinkedList<Integer>();
+                string_list.add(word_index);
                 outer_map.put(inner_key, string_list);
             }
         }
     }
 
     // NOTE: a is modified
-    private void intersect(Set<String> a, Set<String> b) {
+    private void intersect(Set<Integer> a, Set<Integer> b) {
         a.retainAll(b);
     }
 
-    private Map<String, Map<String, List<String>>> word_map;
+    private Map<String, Map<String, List<Integer>>> word_map;
+    private String[] dictionary;
 }
